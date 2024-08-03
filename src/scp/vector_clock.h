7a37c1bdbec06cb5c2e316da1230f6fb90ac0070
MShayanNazeer <muhammadshayannazeer@gmail.com>
2024-08-03 04:14:36 -0400

    minor fixes

--- src/scp/vector_clock.h
+++ src/scp/vector_clock.h
@@ -10,7 +10,7 @@
 #define NUM_NODES   // Adjust this to the number of network functions
 
 typedef struct {
-    int clocks[MAX_NODES];
+    int clocks[NUM_NODES];
 } vector_clock_t;
 
 typedef struct {
@@ -21,6 +21,7 @@ typedef struct {
 } node_clock_map_t;
 
 vector_clock_t *vector_clock_create();
+void vector_clock_destroy(vector_clock_t *vc);
 static int get_node_id(const char *node_name);
 node_clock_map_t *get_node_clock(const char *node_name);
 void vector_clock_update(const char *sender, const char *receiver);
