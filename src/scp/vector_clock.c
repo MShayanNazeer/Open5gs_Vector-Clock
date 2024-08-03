82af3f57557b84cda128e4fc7c6e4c7da489b9e0
MShayanNazeer <muhammadshayannazeer@gmail.com>
2024-08-03 06:01:36 -0400

    sd

--- src/scp/vector_clock.c
+++ src/scp/vector_clock.c
@@ -1,11 +1,15 @@
 #include "vector_clock.h"
 
+static node_clock_map_t *node_clock_map = NULL;
 
-const char *nf_typeArray[] =  { "NULL", "NRF", "UDM", "AMF", "SMF", "AUSF", "NEF", "PCF", "NSSF", "UDR", "LMF", "SEPP", "UPF", "N3IWF", "AF", "UDSF", "BSF", "CHF", "NWDAF", "PCSCF", "HSS", "UCMF", "MME", "SCP"};
+const char *nf_typeArray[] =  { 
+    "NULL", "NRF", "UDM", "AMF", "SMF", "AUSF", "NEF", "PCF", "NSSF", 
+    "UDR", "LMF", "SEPP", "UPF", "N3IWF", "AF", "UDSF", "BSF", "CHF", 
+    "NWDAF", "PCSCF", "HSS", "UCMF", "MME", "SCP"
+};
 
 static int get_node_id(const char *node_name) {
-    int i;
-    for (i = 0; i < NUM_NODES; i++) {
+    for (int i = 0; i < NUM_NODES; i++) {
         if (strcmp(node_name, nf_typeArray[i]) == 0) {
             return i;
         }
@@ -24,39 +28,70 @@ void vector_clock_destroy(vector_clock_t *vc) {
 }
 
 node_clock_map_t *get_node_clock(const char *node_name) {
-    node_clock_map_t *s;
-    s = (node_clock_map_t *)malloc(sizeof(node_clock_map_t));
-    strncpy(s->node_name, node_name, sizeof(s->node_name) - 1);
-    s->node_name[sizeof(s->node_name) - 1] = '\0';
-    s->node_id = get_node_id(node_name);
-    s->vector_clock = *vector_clock_create();
-        
-    return s;
+    node_clock_map_t *current = node_clock_map;
+    while (current != NULL) {
+        if (strcmp(current->node_name, node_name) == 0) {
+            return current;
+        }
+        current = current->next;
+    }
+
+    // If node clock not found, create a new one
+    node_clock_map_t *new_node = (node_clock_map_t *)malloc(sizeof(node_clock_map_t));
+    strncpy(new_node->node_name, node_name, sizeof(new_node->node_name) - 1);
+    new_node->node_name[sizeof(new_node->node_name) - 1] = '\0';
+    new_node->node_id = get_node_id(node_name);
+    new_node->vector_clock = *vector_clock_create();
+    new_node->next = node_clock_map;
+    node_clock_map = new_node;
+
+    return new_node;
 }
 
 void vector_clock_update(const char *sender, const char *receiver) {
-
-    printf("Sender is: %s", sender);
-    printf("Reciever is: %s", receiver);
+    printf("Sender is: %s\n", sender);
+    printf("Receiver is: %s\n", receiver);
+    
     node_clock_map_t *sender_clock = get_node_clock(sender);
     node_clock_map_t *receiver_clock = get_node_clock(receiver);
 
     sender_clock->vector_clock.clocks[sender_clock->node_id]++;
     receiver_clock->vector_clock.clocks[receiver_clock->node_id]++;    
 
-    int i;
-    for (i = 0; i < NUM_NODES; i++) {
+    for (int i = 0; i < NUM_NODES; i++) {
         if (sender_clock->vector_clock.clocks[i] < receiver_clock->vector_clock.clocks[i]) {
             sender_clock->vector_clock.clocks[i] = receiver_clock->vector_clock.clocks[i];
         }
     }
+}
 
-
-    printf("Vector Clock for node %s:\n", sender_clock->node_name);
-    int j;
-    for (j = 0; j < NUM_NODES; j++) {
-        printf("%d ", sender_clock->vector_clock.clocks[j]);
+void vector_clock_merge(int *clock1, int *clock2) {
+    for (int i = 0; i < NUM_NODES; i++) {
+        if (clock1[i] < clock2[i]) {
+            clock1[i] = clock2[i];
+        }
     }
-    printf("\n");
+}
 
+void print_vector_clock(const char *node_name) {
+    node_clock_map_t *node_clock = get_node_clock(node_name);
+    if (node_clock) {
+        printf("Vector Clock for node %s:\n", node_clock->node_name);
+        for (int j = 0; j < NUM_NODES; j++) {
+            printf("%d ", node_clock->vector_clock.clocks[j]);
+        }
+        printf("\n");
+    } else {
+        printf("Node %s not found.\n", node_name);
+    }
+}
+
+int main() {
+    vector_clock_update("AMF", "SMF");
+    vector_clock_update("SMF", "AMF");
+
+    print_vector_clock("AMF");
+    print_vector_clock("SMF");
+
+    return 0;
 }
--- src/scp/vector_clock.h
+++ src/scp/vector_clock.h
@@ -1,26 +1,28 @@
 #ifndef VECTOR_CLOCK_H
 #define VECTOR_CLOCK_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-#define NUM_NODES 24   // Adjust this to the number of network functions
+#define NUM_NODES 24 // Adjust this based on the number of nodes in nf_typeArray
 
 typedef struct {
     int clocks[NUM_NODES];
 } vector_clock_t;
 
-typedef struct {
+typedef struct node_clock_map {
     char node_name[50];
     int node_id;
     vector_clock_t vector_clock;
+    struct node_clock_map *next;
 } node_clock_map_t;
 
 vector_clock_t *vector_clock_create(void);
 void vector_clock_destroy(vector_clock_t *vc);
-static int get_node_id(const char *node_name);
 node_clock_map_t *get_node_clock(const char *node_name);
 void vector_clock_update(const char *sender, const char *receiver);
+void vector_clock_merge(int *clock1, int *clock2);
+void print_vector_clock(const char *node_name);
 
 #endif // VECTOR_CLOCK_H
