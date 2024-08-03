02003202247ae60911bf5e3bdc7b63a47d61739f
MShayanNazeer <muhammadshayannazeer@gmail.com>
2024-08-03 05:04:59 -0400

    sdf

--- src/scp/vector_clock.c
+++ src/scp/vector_clock.c
@@ -37,7 +37,7 @@ node_clock_map_t *get_node_clock(const char *node_name) {
         nf_clock_dictionary[s->node_id].node_id = get_node_id(node_name); 
         memcpy(*nf_clock_dictionary[s->node_id].node_name, node_name, sizeof(node_name)-1);
         // nf_clock_dictionary[s->node_id].vector_clock= s->vector_clock;
-        memcpy(nf_clock_dictionary[s->node_id].vector_clock, s->vector_clock, sizeof(vector_clock_t));
+        memcpy(*nf_clock_dictionary[s->node_id].vector_clock, s->vector_clock, sizeof(vector_clock_t));
     }
     return s;
 }
